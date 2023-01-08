09565fe7ac01e14ceca7879b2d87303682d5672e
Mike Rachman <mikerachman1@gmail.com>
2023-01-08 14:23:13 -0500

    added counter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Clock from './MyComponent';
 import Toggle from './Toggle';
+import Counter from './Counter';
 
 function App() {
   return (
     <div>
       <Clock title="Mikes Site" />
       <Toggle />
+      <Counter />
     </div>
   )
 }
