2355bd2296dbf303c389b41d364a6263b4f8ddda
Mike Rachman <mikerachman1@gmail.com>
2023-01-08 14:16:03 -0500

    added toggle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Clock from './MyComponent';
+import Toggle from './Toggle';
 
 function App() {
   return (
     <div>
       <Clock title="Mikes Site" />
+      <Toggle />
     </div>
   )
 }
